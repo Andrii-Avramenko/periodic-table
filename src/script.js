e15e667295388fd05ab50fb05d3606ab5f40e63b
Andrii-Avramenko <andreitvmm228@gmail.com>
2026-03-10 19:25:38 +0000

    periodic table layout

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 import periodicTable from "./PeriodicTable.json";
 
-const container = document.querySelector(".container");
+const container = document.querySelector(".p-table");
 
 periodicTable.elements.forEach((element) => {
+  if (element.category == "lanthanide" || element.category == "actinide") {
+    return
+  }
   container.insertAdjacentHTML(
     "beforeend",
-    `<div class="element group-${element.group}">
+    `<div class="element group-${element.group}" style="--group: ${element.group};" data-name="${element.name}" data-category="${element.category}">
             <p class="element-symbol">${element.symbol}</p>
             <p class="element-name">${element.name}</p>
         </div>`,
